b7af6dd1a21391ca690f1694eada7d3ced496ab2
Hindmet29 <97840456+Hindmet29@users.noreply.github.com>
2022-04-10 16:47:04 +0200

    Create jeu.py

---
+++ jeu.py
@@ -0,0 +1,34 @@
+class game:
+    
+
+def création_joueur ():
+    joueur1=input("J1, entrez votre prénom :")
+    joueur2=input("J2, entrez votre prénom :")
+    return joueur1,joueur2
+
+def demande_colonne()->bool :
+    
+
+def poser_jeton (création_joueur, tab , demande_colonne )->tab :
+    
+    """
+  resultat=True
+    L=[]
+    for c in range(len(g)+1):
+        for l in range(len(g)):
+            while resultat==False or l==len(g):
+                if g[c][l] == 0:
+                    resultat=True
+                else :
+                    resultat=False
+        if resultat==True :
+            print("La colonne",c+1,"est libre.")
+            L.append(True)
+        else :
+            print("La colonne",c+1,"n'est pas libre.")
+            L.append(False)
+    return L
+    """
+    
+    
+def est_gagnant (tab)-> 0,1,2 :
