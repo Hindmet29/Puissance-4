4a60882a38434800c55fc03d115a5e40e059b93d
Hindmet29 <97840456+Hindmet29@users.noreply.github.com>
2022-04-10 17:25:04 +0200

    Update jeu.py

--- jeu.py
+++ jeu.py
@@ -11,24 +11,5 @@ def demande_colonne()->bool :
 
 def poser_jeton (création_joueur, tab , demande_colonne )->tab :
     
-    """
-  resultat=True
-    L=[]
-    for c in range(len(g)+1):
-        for l in range(len(g)):
-            while resultat==False or l==len(g):
-                if g[c][l] == 0:
-                    resultat=True
-                else :
-                    resultat=False
-        if resultat==True :
-            print("La colonne",c+1,"est libre.")
-            L.append(True)
-        else :
-            print("La colonne",c+1,"n'est pas libre.")
-            L.append(False)
-    return L
-    """
-    
     
 def est_gagnant (tab)-> 0,1,2 :
