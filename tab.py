cb61fca99edbd444221b0c145ed1da6d9f8d76ab
Hindmet29 <97840456+Hindmet29@users.noreply.github.com>
2022-04-10 16:46:10 +0200

    Update and rename Programme to tab.py

---
+++ tab.py
@@ -0,0 +1,25 @@
+import random
+
+# Structure de donnée mémorisant la grille : une liste à 2 dimensions (6 lignes et 7 colonnes) contenant :
+# 0= colone vide , 1 = jeton joueur1 , 2 = jeton joueur2
+tab_colonne=7*[0]
+
+class tab :
+    
+
+def __init__(self):
+        self.tab =
+         [[0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0],
+          [0,1,2,0,0,0,0]]
+
+
+def vérifie_horizontalemen(self):
+    
+def vérifie_verticalement(self):
+    
+def vérifie_diagonalement(self):
+     
