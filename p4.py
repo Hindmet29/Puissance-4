63518028de9cde6d5add71b9835e16dc5ff9ab8e
Hindmet29 <97840456+Hindmet29@users.noreply.github.com>
2022-05-20 13:23:54 +0200

    Update p4.py

--- p4.py
+++ p4.py
@@ -1,4 +1,4 @@
-class Game:
+classclass Game:
     
     def __init__(self) :# Création de la grille de jeu
         self.grille = [[0] * 7 for i in range (6)]  
@@ -6,17 +6,17 @@ class Game:
         self.joueur2 = input("J2, entrez votre prénom :")
 
         
-    def poser_jeton(self, joueur, colonne):
+    def poser_jeton(self, joueur, colonne):#crétaion de la fonction pour deposer le jeton 
         for ligne in range(7):
             if "0" in self.grille[colone]:
                 if self.grille[colone][ligne] == "0":
                     self.grille[colone][ligne] = joueur
                     break
         
-    def est_gagnant (self)->int:
+    def est_gagnant (self)->int:# déterminer le gagnant 
         trouve=0
         
-    def est_gagnant_horiz(self)-> int :
+    def est_gagnant_horiz(self)-> int :#fonction qui determine  si il y a 4pions aligniés sur une ligne 
         for ligne in self.grille:
             joueur = 0
             repetition = 0
@@ -29,53 +29,47 @@ class Game:
                 if repetition == 3:
                     return joueur
     
-    def est_gagnant_vert(self)-> int :
+    def est_gagnant_vert(self)-> int :#fonction qui determine  si il y a 4pions aligniés sur une colone  
         for c in range (len(self.grille[0])) :
             joueur = 0
             repetition = 0
             for l in range(len(self.grille)):
                 if self.grille [l][c] == joueur and self.grille [l][c] != 0 :
-                    repetition += 1
+                    repetition += 1#rajouter a chaque fois qu'il y a un pions 
                 else :
                     joueur = self.grille [l][c]
                     repetition =  0
                 if repetition == 3:
-                    return joueur
+                    return joueur#affiche le nom du joueur si il a 4 pions alignés 
         return 0
                     
                     
-    def est_gagnant_diag_SE(self)-> int:
-        nb_colonne=7
-        nb_ligne =6
-        i=0
-        for ligne[0] in  range (len(self.grille[0])) :
-            i+= 1
-            case = [ligne+1][1]
-            joueur = 0
-            repetition = 0
-            for colonne [0] in range (len(self.grille[0])):
-                if self.grille [colonne[0]][ligne[0]]== joueur and self.grille [colonne[0]][ligne[0]] != 0 :
-                    repetition += 1
-                else :
-                    joueur = self.grille [colonne[0]][ligne[0]]
-                    repetition =  0
-            if repetition == 3:
-                return joueur
-        return 0
-                
-        
+    def est_gagnant_diag_SE(self)-> int:#fonction qui determine  si il y a 4pions aligniés sur une diagonale niveau Sud Est 
+        if self.grille[0][0]== self.grille[0][1]== self.grille[0][2]==self.grille[0][3]=="1 ":
+            return "le joueur 1 gagne cette manche"
+        elif self.grille[0][0]== self.grille[0][1]== self.grille[0][2]==self.grille[0][3]=="2":
+            return "le joueur 2 gagne cette manche"
+
+        elif self.grille[0][0]== self.grille[0][1]== self.grille[0][2]==self.grille[0][3]=="1 ":
+            return "le joueur 1 gagne cette manche"
+        elif self.grille[0][0]== self.grille[0][1]== self.grille[0][2]==self.grille[0][3]=="2 ":
+            return "le joueur 2 gagne cette manche"
+            
     
-    def est_gagnant_diag_NE(self)-> int :
-        
+    """def est_gagnant_diag_NE(self)-> int :#fonction qui determine  si il y a 4pions aligniés sur une diagonale niveau Nord Ouest 
         return None
-"""
+        """
+#jeu de test
+""
 jeu= Game()
 print(jeu.grille)
 jeu.grille[5] = [1,1,2,2,2,2,1]
 print(jeu.grille)
 print(jeu.est_gagnant_horiz())
+""
+
 """
-"""jeu= Game()
+jeu= Game()
 print(jeu.grille)
 jeu.grille = [[0,0,0,0,0,0,0],
               [0,0,0,0,0,0,0],
@@ -91,6 +85,7 @@ jeu.grille[2][6] = 1
 print(jeu.grille)
 print(jeu.est_gagnant_vert())
 """
+"""
 jeu= Game()
 print(jeu.grille)
 jeu.grille = [[0,0,0,0,0,0,0],
@@ -105,4 +100,7 @@ print(jeu.est_gagnant_diag_SE())
 
 jeu.grille[3][3] = 1
 print(jeu.grille)
+
 print(jeu.est_gagnant_diag_SE())
+
+"""
